b5c6aed777d88057221cfe8b2da9aaefc9479d13
naveen selvam <naveen.selvam@thirvusoft.in>
2021-12-17 12:16:57 +0530

    dynamic filter

---
+++ thirvusoft/thirvusoft/report/salary_slip1/salary_slip1.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Salary Slip1"] = {
+	"filters": [
+
+	]
+};
